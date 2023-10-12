752caa06697d2c0d4f88d6dade529bf61e9b46fa
hyx <huangyx1229@163.com>
2023-10-12 14:41:38 +0800

    docs: 获取配置文件变量名

--- build/getConfigFileName.ts
+++ build/getConfigFileName.ts
@@ -1,9 +1,13 @@
 /**
- * Get the configuration file variable name
+ * 获取配置文件变量名
  * @param env
  */
 export const getConfigFileName = (env: Record<string, any>) => {
-  return `__PRODUCTION__${env.VITE_GLOB_APP_SHORT_NAME || '__APP'}__CONF__`
-    .toUpperCase()
-    .replace(/\s/g, '');
-};
+  return (
+    `__PRODUCTION__${env.VITE_GLOB_APP_SHORT_NAME || '__APP'}__CONF__`
+      // 转成大写
+      .toUpperCase()
+      // 将空白换成空字符串
+      .replace(/\s/g, '')
+  )
+}
