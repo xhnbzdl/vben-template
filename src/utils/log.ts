d660190bb1df945687d70416c4cd487f03acb5d5
hyx <huangyx1229@163.com>
2023-10-13 10:37:03 +0800

    docs: 日志打印工具

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,9 +1,14 @@
+// 日志打印相关方法
+
+// 项目名称
 const projectName = import.meta.env.VITE_GLOB_APP_TITLE
 
+/** 打印警告日志 */
 export function warn(message: string) {
   console.warn(`[${projectName} warn]:${message}`)
 }
 
+/** 打印错误日志 */
 export function error(message: string) {
   throw new Error(`[${projectName} error]:${message}`)
 }
