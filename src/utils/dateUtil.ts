cd879793e7480c96633a197a12898a599ff0f856
hyx <huangyx1229@163.com>
2023-10-17 14:13:21 +0800

    docs: 处理时间格式化的工具类

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -1,11 +1,13 @@
 /**
- * Independent time operation tool to facilitate subsequent switch to dayjs
+ * 独立的时间操作工具，便于切换到 dayjs
  */
 import dayjs from 'dayjs'
 
+// 时间格式
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss'
 const DATE_FORMAT = 'YYYY-MM-DD'
 
+/** 格式化时间(YYYY-MM-DD HH:mm:ss) */
 export function formatToDateTime(
   date: dayjs.Dayjs | undefined = undefined,
   format = DATE_TIME_FORMAT,
@@ -13,6 +15,7 @@ export function formatToDateTime(
   return dayjs(date).format(format)
 }
 
+/** 格式化时间(YYYY-MM-DD) */
 export function formatToDate(
   date: dayjs.Dayjs | undefined = undefined,
   format = DATE_FORMAT,
