6337d39d19a2050cf18e284db227a72027c47270
hyx <huangyx1229@163.com>
2023-10-17 17:52:19 +0800

    docs: 配置基础路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,9 @@ import type { App } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { basicRoutes } from './routes'
 
-// 白名单应该包含基本静态路由
+/** 白名单应该包含基本静态路由 */
 const WHITE_NAME_LIST: string[] = []
+// 将所有基础路由的路由名称加入白名单
 const getRouteNames = (array: any[]) =>
   array.forEach((item) => {
     WHITE_NAME_LIST.push(item.name)
@@ -13,8 +14,7 @@ const getRouteNames = (array: any[]) =>
   })
 getRouteNames(basicRoutes)
 
-// app router
-// 创建一个可以被 Vue 应用程序使用的路由实例
+/** 创建一个可以被 Vue 应用程序使用的路由实例 */
 export const router = createRouter({
   // 创建一个 hash 历史记录。
   history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
@@ -25,18 +25,20 @@ export const router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
 })
 
-// reset router
+/** 重置路由 */
 export function resetRouter() {
   router.getRoutes().forEach((route) => {
+    // 路由名称
     const { name } = route
+    // 路由不在白名单内
     if (name && !WHITE_NAME_LIST.includes(name as string)) {
+      // 删除路由
       router.hasRoute(name) && router.removeRoute(name)
     }
   })
 }
 
-// config router
-// 配置路由器
+/** 配置路由器 */
 export function setupRouter(app: App<Element>) {
   app.use(router)
 }
