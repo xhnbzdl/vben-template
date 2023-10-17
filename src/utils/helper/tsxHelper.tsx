454faa63230e6d7d1abd53b485f7532c91a41de0
hyx <huangyx1229@163.com>
2023-10-17 11:31:12 +0800

    docs: 处理vue插槽的工具类

--- src/utils/helper/tsxHelper.tsx
+++ src/utils/helper/tsxHelper.tsx
@@ -2,28 +2,37 @@ import { Slots } from 'vue'
 import { isFunction } from '/@/utils/is'
 
 /**
- * @description:  Get slot to prevent empty error
+ * 从Vue 3组件的slots中获取具名插槽的函数
+ * @description:  获取插槽以防止空错误
+ * @param data 用于向插槽传递数据
  */
 export function getSlot(slots: Slots, slot = 'default', data?: any) {
+  // 检查 slots 是否为b null 或是否具有名为 `slot` 变量值的插槽
   if (!slots || !Reflect.has(slots, slot)) {
     return null
   }
+  // 检查指定插槽是否为一个函数
   if (!isFunction(slots[slot])) {
     console.error(`${slot} is not a function!`)
     return null
   }
   const slotFn = slots[slot]
-  if (!slotFn) return null
+  if (!slotFn) {
+    return null
+  }
+
   return slotFn(data)
 }
 
 /**
- * extends slots
+ * 获取指定排除项以外的插槽
  * @param slots
- * @param excludeKeys
+ * @param excludeKeys 要排除的插槽（传插槽名称数组）
  */
 export function extendSlots(slots: Slots, excludeKeys: string[] = []) {
+  //  slots 对象中所有的插槽名称
   const slotKeys = Object.keys(slots)
+  // 处理后的插槽
   const ret: any = {}
   slotKeys.map((key) => {
     if (excludeKeys.includes(key)) {
