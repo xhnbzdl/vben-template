9358a209f746888b81235aa21783f07c9f9e8365
hyx <huangyx1229@163.com>
2023-10-18 10:15:39 +0800

    docs: 一些工具方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,14 +7,16 @@ import { isObject } from '/@/utils/is'
 export const noop = () => {}
 
 /**
- * @description:  Set ui mount node
+ * 用于确定弹出框容器的实用函数
+ * 确定在哪个 HTML 元素内部渲染弹出框或浮动元素，以确保正确的布局和样式
+ * @description:  设置 ui 载入节点
  */
 export function getPopupContainer(node?: HTMLElement): HTMLElement {
   return (node?.parentNode as HTMLElement) ?? document.body
 }
 
 /**
- * Add the object as a parameter to the URL
+ * 将对象作为参数添加到 URL 中
  * @param baseUrl url
  * @param obj
  * @returns {string}
@@ -32,7 +34,13 @@ export function setObjToUrlParams(baseUrl: string, obj: any): string {
   return /\?$/.test(baseUrl) ? baseUrl + parameters : baseUrl.replace(/\/?$/, '?') + parameters
 }
 
-// 深度合并
+/**
+ * 深度合并
+ * @param src 源对象
+ * @param target 要合并的目标对象，合并时目标对象的值可覆盖源对象的值
+ * @description: 浅合并{...src,...target}只会将源对象和目标对象的顶级属性合并，不会递归处理嵌套对象，会导致源对象的一些属性丢失
+ * @returns
+ */
 export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
   let key: string
   for (key in target) {
@@ -62,7 +70,11 @@ export function openWindow(
   window.open(url, target, feature.join(','))
 }
 
-// dynamic use hook props
+/**
+ * 将包含 Ref 对象的 props 转换为普通的 JavaScript 对象，以便更容易操作
+ * @param props
+ * @returns
+ */
 export function getDynamicProps<T, U>(props: T): Partial<U> {
   const ret: Recordable = {}
 
@@ -73,6 +85,12 @@ export function getDynamicProps<T, U>(props: T): Partial<U> {
   return ret as Partial<U>
 }
 
+/**
+ * 用于获取一个 RouteLocationNormalized 类型对象的原始版本。
+ * 它会去除 matched 属性中的一些属性，只保留了 meta、name 和 path 属性
+ * @param route
+ * @returns
+ */
 export function getRawRoute(route: RouteLocationNormalized): RouteLocationNormalized {
   if (!route) return route
   const { matched, ...opt } = route
@@ -88,6 +106,12 @@ export function getRawRoute(route: RouteLocationNormalized): RouteLocationNormal
   }
 }
 
+/**
+ * 将组件注册到 Vue 应用中
+ * @param component
+ * @param alias
+ * @returns
+ */
 export const withInstall = <T>(component: T, alias?: string) => {
   const comp = component as any
   comp.install = (app: App) => {
